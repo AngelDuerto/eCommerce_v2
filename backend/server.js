c0b56f979607ea4a86cd4b3ad02f856cc653d643
AngelDuerto <ajdupe08@gmail.com>
2025-01-28 21:41:28 -0500

    fixes to server

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 
 // Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
